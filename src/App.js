ed645ec1c75198fceeadbe4d52c78d6f0b5fd3d6
ayushraj706 <ayushrajayushhh@gmail.com>
2026-02-09 14:58:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from '@mui/material';
 
+// Purane components wahi hain
 import { ChannelDetail, VideoDetail, SearchFeed, Navbar, Feed } from './components';
+
+// Naya Landing Page import kiya (Dhyaan dena yahan)
+import LandingPage from './components/LandingPage'; 
 
 const App = () => (
   <BrowserRouter>
     <Box sx={{ backgroundColor: '#000' }}>
       <Navbar />
       <Routes>
-        <Route exact path='/' element={<Feed />} />
+        {/* 1. Jab koi site kholega, toh Landing Page dikhega */}
+        <Route exact path='/' element={<LandingPage />} />
+
+        {/* 2. Button dabane par wo yahan aayega (Video Feed) */}
+        <Route path='/feed' element={<Feed />} />
+
+        {/* Baaki sab same hai */}
         <Route path='/video/:id' element={<VideoDetail />} />
         <Route path='/channel/:id' element={<ChannelDetail />} />
         <Route path='/search/:searchTerm' element={<SearchFeed />} />
