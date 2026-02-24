db06c589cdffd0d3aad4d28c241ec2b3a61de127
ayushraj706 <ayushrajayushhh@gmail.com>
2026-02-25 00:35:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from '@mui/material';
 
-import { ChannelDetail, VideoDetail, SearchFeed, Navbar, Feed, PlaylistDetail } from './components';
+// Naye Shorts aur BottomNav ko import kiya
+import { ChannelDetail, VideoDetail, SearchFeed, Navbar, Feed, PlaylistDetail, Shorts, BottomNav } from './components';
 import LandingPage from './components/LandingPage'; 
 
 const App = () => (
   <BrowserRouter>
-    <Box sx={{ backgroundColor: '#000', minHeight: '100vh' }}>
+    {/* pb: '70px' isliye diya taaki Bottom Nav ke piche ka content chhupe nahi */}
+    <Box sx={{ backgroundColor: '#000', minHeight: '100vh', pb: '70px' }}>
       <Navbar />
       
       <Routes>
-        {/* Yahan 'exact' lagana zaroori hai, taaki ye sirf home page par dikhe */}
         <Route exact path='/' element={<LandingPage />} />
         
         <Route path='/feed' element={<Feed />} />
+        
+        {/* NAYA: Shorts Route */}
+        <Route path='/shorts' element={<Shorts />} />
+
         <Route path='/video/:id' element={<VideoDetail />} />
         <Route path='/channel/:id' element={<ChannelDetail />} />
         <Route path='/playlist/:id' element={<PlaylistDetail />} />
         <Route path='/search/:searchTerm' element={<SearchFeed />} />
       </Routes>
+
+      {/* NAYA: Bottom Navigation Patti */}
+      <BottomNav />
+      
     </Box>
   </BrowserRouter>
 );
 
 export default App;
-  
