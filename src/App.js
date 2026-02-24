af0d714738fc769596cc6ba0516b214f477b38d0
ayushraj706 <ayushrajayushhh@gmail.com>
2026-02-25 00:01:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from '@mui/material';
+
 import { ChannelDetail, VideoDetail, SearchFeed, Navbar, Feed, PlaylistDetail } from './components';
 import LandingPage from './components/LandingPage'; 
 
-const App = () => {
-  return (
-    <BrowserRouter>
-      <Box sx={{ backgroundColor: '#000', minHeight: '100vh' }}>
-        <Navbar />
-        <Routes>
-          <Route exact path='/' element={<LandingPage />} />
-          <Route path='/feed' element={<Feed />} />
-          
-          {/* Isme humne VideoPlayer wala logic integrate kiya hai */}
-          <Route path='/video/:id' element={<VideoDetail />} />
-          
-          <Route path='/channel/:id' element={<ChannelDetail />} />
-          <Route path='/playlist/:id' element={<PlaylistDetail />} />
-          <Route path='/search/:searchTerm' element={<SearchFeed />} />
-        </Routes>
-      </Box>
-    </BrowserRouter>
-  );
-};
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: '#000', minHeight: '100vh' }}>
+      <Navbar />
+      
+      {/* SAARA KHEL YAHAN HAI: Routes ke andar hi sab kuch hona chahiye */}
+      <Routes>
+        {/* Sirf jab link ayus.fun/ hoga, tabhi Landing Page dikhega */}
+        <Route exact path='/' element={<LandingPage />} />
+
+        {/* Baaki sab apne-apne link par dikhenge */}
+        <Route path='/feed' element={<Feed />} />
+        <Route path='/video/:id' element={<VideoDetail />} />
+        <Route path='/channel/:id' element={<ChannelDetail />} />
+        <Route path='/playlist/:id' element={<PlaylistDetail />} />
+        <Route path='/search/:searchTerm' element={<SearchFeed />} />
+      </Routes>
+      
+    </Box>
+  </BrowserRouter>
+);
 
 export default App;
